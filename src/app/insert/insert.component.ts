988ac677fb9b03a5f9bbc5ee397251601572371d
NontachaiComsci <635021000081@mail.rmutk.ac.th>
2023-11-28 12:32:19 +0700

    edit

--- src/app/insert/insert.component.ts
+++ src/app/insert/insert.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-import { EmployeeService } from '../employee.service';
-
 @Component({
   selector: 'app-insert',
   standalone: true,
